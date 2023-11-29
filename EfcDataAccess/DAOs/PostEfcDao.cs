41e9304459eeb1de928aa7f74544fe7272cd9a7a
BakerBella <131524741+BakerBella@users.noreply.github.com>
2023-11-29 13:03:15 +0100

    Add files via upload

---
+++ EfcDataAccess/DAOs/PostEfcDao.cs
@@ -0,0 +1,63 @@
+﻿using Application.DaoInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EfcDataAccess.DAOs;
+
+public class PostEfcDao : IPostDao
+{
+    private readonly PostContext context;
+
+    public PostEfcDao(PostContext context)
+    {
+        this.context = context;
+    }
+    public async Task<Post> CreateAsync(Post post)
+    {
+        EntityEntry<Post> added = await context.Posts.AddAsync(post);
+        await context.SaveChangesAsync();
+        return added.Entity;
+    }
+
+    public async Task<IEnumerable<Post>> GetAsync(SearchPostParametersDto searchParameters)
+    {
+        IQueryable<Post> query = context.Posts.Include(post => post.Owner).AsQueryable();
+        
+        if (!string.IsNullOrEmpty(searchParameters.OwnerUsername))
+        {
+            query = query.Where(post =>
+                post.OwnerUsername.ToLower().Equals(searchParameters.OwnerUsername.ToLower()));
+        }
+        
+        List<Post> result = await query.ToListAsync();
+        return result;
+    }
+
+    public async Task UpdateAsync(Post post)
+    {
+        context.Posts.Update(post);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<Post?> GetByIdAsync(int id)
+    {
+        Post? found = await context.Posts
+            .Include(post => post.Owner)
+            .SingleOrDefaultAsync(post => post.Id == id);
+        return found;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        Post? existing = await GetByIdAsync(id);
+        if (existing == null)
+        {
+            throw new Exception($"Post with id {id} not found");
+        }
+
+        context.Posts.Remove(existing);
+        await context.SaveChangesAsync();
+    }
+}
