469554c9f53b852fcd463650c872b838614ebf8a
BakerBella <131524741+BakerBella@users.noreply.github.com>
2023-11-29 13:01:24 +0100

    Add files via upload

---
+++ Domain/DTOs/PostBasicDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.DTOs;
+
+public class PostBasicDto
+{
+    public int Id { get; }
+    public string OwnerUsername { get;  }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    
+    public PostBasicDto(int id, string ownerUsername, string title, string body)
+    {
+        Id = id;
+        OwnerUsername = ownerUsername;
+        Title = title;
+        Body = body;
+    }
+}
