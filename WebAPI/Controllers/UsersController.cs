70c6237ff960feb043a5e2cc03caad0bc7b3d78b
BakerBella <131524741+BakerBella@users.noreply.github.com>
2023-12-01 14:40:01 +0100

    Add files via upload

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -11,10 +11,12 @@ namespace WebAPI.Controllers;
 public class UsersController : ControllerBase
 {
     private readonly IUserLogic userLogic;
+    private readonly ICommentLogic commentLogic;
 
-    public UsersController(IUserLogic userLogic)
+    public UsersController(IUserLogic userLogic, ICommentLogic commentLogic)
     {
         this.userLogic = userLogic;
+        this.commentLogic = commentLogic;
     }
     
     [HttpPost]
@@ -40,6 +42,21 @@ public class UsersController : ControllerBase
             SearchUserParametersDto parameters = new(username);
             IEnumerable<User> users = await userLogic.GetAsync(parameters);
             return Ok(users);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+    [HttpGet("{username}/comments")]
+    public async Task<ActionResult<IEnumerable<Comment>>> GetCommentsForUserAsync([FromRoute] string username)
+    {
+        try
+        {
+            var comments = await commentLogic.GetCommentsForUserAsync(username);
+            return Ok(comments);
         }
         catch (Exception e)
         {
